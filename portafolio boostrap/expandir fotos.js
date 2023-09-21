1cdcae82da31a8b779e119b23eb9963e9c59ea9f
josuequincha <145625682+josuequincha@users.noreply.github.com>
2023-09-20 19:43:50 -0500

    Delete portafolio boostrap directory

--- portafolio boostrap/expandir fotos.js
+++
@@ -1,9 +0,0 @@
-const fulimgbox = document.getElementById("fulimgbox");
-fulimg = document.getElementById("fulimg")
-function openFulImg(reference){
-    fulimgbox.style.display = "flex";
-    fulimg.src = reference
-}
-function closeclip(){
-    fulimgbox.style.display ="none";
-}
